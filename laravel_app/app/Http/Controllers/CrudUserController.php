5aff56e70282c75a0889eb604596a0943c59f578
ThCong1407 <thanhcong14072004@gmail.com>
2024-04-06 09:16:46 +0700

    login

---
+++ laravel_app/app/Http/Controllers/CrudUserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * CRUD User controller
+ */
+class CrudUserController extends Controller
+{
+
+    /**
+     * Login page
+     */
+    public function login()
+    {
+        return view('crud_user.login');
+    }
+
+    /**
+     * User submit form login
+     */
+    public function authUser(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('list')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+}
