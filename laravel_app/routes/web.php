e36ed221ca0d1eee48d2861055eaf8ddd69484f1
TATTRAN25 <huytran998811@gmail.com>
2024-04-11 20:42:19 +0700

    haha

--- laravel_app/routes/web.php
+++ laravel_app/routes/web.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -15,24 +14,10 @@ use App\Http\Controllers\CrudUserController;
 |
 */
 
-Route::get('dashboard', [CrudUserController::class, 'dashboard']);
 
-Route::get('login', [CrudUserController::class, 'login'])->name('login');
-Route::post('login', [CrudUserController::class, 'authUser'])->name('user.authUser');
-
-Route::get('create', [CrudUserController::class, 'createUser'])->name('user.createUser');
-Route::post('create', [CrudUserController::class, 'postUser'])->name('user.postUser');
-
-Route::get('read', [CrudUserController::class, 'readUser'])->name('user.readUser');
-
-Route::get('delete', [CrudUserController::class, 'deleteUser'])->name('user.deleteUser');
-
-Route::get('update', [CrudUserController::class, 'updateUser'])->name('user.updateUser');
-Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user.postUpdateUser');
 
 Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
 
-Route::get('signout', [CrudUserController::class, 'signOut'])->name('signout');
 
 Route::get('/', function () {
     return view('welcome');
